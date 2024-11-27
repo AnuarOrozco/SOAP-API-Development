a84c9a8803fad9e622f7be0f91b44dc5af3f9226
Anuar Orozco <anuarorozzco@gmail.com>
2024-11-27 17:58:59 -0600

    fixed

--- src/main/java/com/soap/wsdl/AddResponse.java
+++ src/main/java/com/soap/wsdl/AddResponse.java
@@ -1,25 +1,16 @@
-//
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.0 
-// Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2024.11.27 a las 05:12:07 PM CST 
-//
-
-
 package com.soap.wsdl;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
-
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
 
 /**
  * <p>Clase Java para anonymous complex type.
- * 
+ *
  * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
- * 
+ *
  * <pre>
  * &lt;complexType&gt;
  *   &lt;complexContent&gt;
@@ -31,12 +22,12 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;/complexContent&gt;
  * &lt;/complexType&gt;
  * </pre>
- * 
- * 
+ *
+ *
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
-    "addResult"
+        "addResult"
 })
 @XmlRootElement(name = "AddResponse")
 public class AddResponse {
@@ -46,7 +37,7 @@ public class AddResponse {
 
     /**
      * Obtiene el valor de la propiedad addResult.
-     * 
+     *
      */
     public int getAddResult() {
         return addResult;
@@ -54,7 +45,7 @@ public class AddResponse {
 
     /**
      * Define el valor de la propiedad addResult.
-     * 
+     *
      */
     public void setAddResult(int value) {
         this.addResult = value;
--- src/main/java/com/soap/wsdl/SubtractResponse.java
+++ src/main/java/com/soap/wsdl/SubtractResponse.java
@@ -2,17 +2,13 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.0 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2024.11.27 a las 05:12:07 PM CST 
+// Generado el: 2024.11.27 a las 05:49:27 PM CST 
 //
 
 
 package com.soap.wsdl;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.*;
 
 
 /**
